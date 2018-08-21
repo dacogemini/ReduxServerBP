af3a1891bee29ba7095bef69736115b7124656ba
Dan <Dan@clarkwc.com>
2018-08-21 11:13:39 -0500

    Added class components

---
+++ Desktop/ReduxSimpleStart-master/src/components/searchbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+//* CLASS Component
+class SearchBar extends Component {
+    // Declare a method on a class
+    //! Every class must have a render function
+    //! which returns JSX
+    render() {
+        return <input />;
+    }
+}
+// FUNCTIONAL Component
+// const SearchBar = () => {
+//        return <input />;
+// };
+export default SearchBar;
