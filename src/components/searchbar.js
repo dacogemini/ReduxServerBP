dc4c2a060ab73b44068167fc1049ffbad3fa6204
Dan Collins <dcollins31@gmail.com>
2018-08-21 21:30:53 -0500

    Added state

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,19 +1,43 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 
 //* CLASS Component
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: '' // << Records 'search term' here
+        };
+    }
+    /*
+       _   _   _   _   _
+      / \ / \ / \ / \ / \
+     ( c | l | a | s | s )
+      \_/ \_/ \_/ \_/ \_/
+    */
     // Declare a method on a class
     //! Every class must have a render function
     //! which returns JSX
+    //========================================
     render() {
-        return <input onChange = {(event) => 
-        console.log(event.target.value)} />;
+        return ( 
+        <div >
+            <input onChange = {
+                (event) =>
+                this.setState // << Update state here
+                ({ term: event.target.value })}/>
+            Value of input: { this.state.term }
+            </div>
+        )
     }
-    //* Event handler
-    //* Event gives access to the input value
-    // onInputChange(event) {
-    //     console.log(event.target.value);
-    // }
+    //========================================
+    // Event handler
+    // Event gives access to the input value
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
 }
 // FUNCTIONAL Component
 // const SearchBar = () => {
