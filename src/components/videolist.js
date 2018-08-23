6ddb7df47a810b252c06f12e8f6a60b2cde7822a
Dan Collins <dcollins31@gmail.com>
2018-08-22 21:09:24 -0500

    Added maps

--- src/components/videolist.js
+++ src/components/videolist.js
@@ -2,7 +2,7 @@ import React from 'react';
 import VideoListItem from './videolist_item';
 
 const VideoList = (props) => {
-    const videoItems = props.videos.map((video) => {
+    const videoItdded mapsems = props.videos.map((video) => {
         return <VideoListItem video={video} />
     })
     
